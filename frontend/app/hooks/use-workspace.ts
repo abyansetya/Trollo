bbe41fd23a0acfb62fee836f493c9d43fe0050e2
abyansetya <abyansetyaa@gmail.com>
2025-11-19 22:22:06 +0700

    fe: update API call for workspace details

--- frontend/app/hooks/use-workspace.ts
+++ frontend/app/hooks/use-workspace.ts
@@ -14,3 +14,10 @@ export const useGetWorkSpacesQuery = () => {
     queryFn: async () => fetchData("/workspaces"),
   });
 };
+
+export const useGetWorkSpaceQuery = (workspaceId: string) => {
+  return useQuery({
+    queryKey: ["workspace", workspaceId],
+    queryFn: async () => fetchData(`/workspaces/${workspaceId}/projects`),
+  });
+};
